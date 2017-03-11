31dfb40c8d0006c485dd8e558298642f2f282817
Allen Boynton <aboynton@fullsail.edu>
2017-03-11 01:20:55 -0500

    Adjusted arrays in sudoku
    Changed from 4 arrays per board to 1 longer one. Easier and more
    efficient because it eleviates arrays within arrays.

--- BoyntonAllen_CE04/app/src/main/java/edu/fullsail/aboynton/boyntonallen_ce04/MainActivity.java
+++ BoyntonAllen_CE04/app/src/main/java/edu/fullsail/aboynton/boyntonallen_ce04/MainActivity.java
@@ -29,14 +29,13 @@ public class MainActivity extends AppCompatActivity {
     private SudokuBoard sudokuBoard3;
 
     // All double int arrays needed for the game...I think???
-    private int[][] userNumbers;
+    private int[] userNumbers;
     private EditText[][] boardViews;
 
     // 3 game boards provided in instructions of project
-    private int[][] gameNumbers1;
-    private int[][] gameNumbers2;
-    private int[][] gameNumbers3;
-//    private int[][][] gameNumbers;
+    private int[] gameNumbers1;
+    private int[] gameNumbers2;
+    private int[] gameNumbers3;
 
     // Arrays for row, column, and corner equality
     private int[][] rowArrays;
@@ -55,19 +54,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "----> onCreate(): ON");
 
-        userNumbers = new int[4][4];
+        userNumbers = new int[16];
 
         // Numbers for board 1
-        gameNumbers1 = new int[][]{{2, 1, 4, 3}, {3, 4, 1, 2}, {4, 3, 2, 1}, {1, 2, 3, 4}};
+        gameNumbers1 = new int[]{2, 1, 4, 3, 3, 4, 1, 2, 4, 3, 2, 1, 1, 2, 3, 4};
 
         // Numbers for board 2
-        gameNumbers2 = new int[][]{{4, 1, 2, 3}, {2, 3, 1, 4}, {3, 2, 4, 1}, {1, 4, 3, 2}};
+        gameNumbers2 = new int[]{4, 1, 2, 3, 2, 3, 1, 4, 3, 2, 4, 1, 1, 4, 3, 2};
 
         // Numbers for board 3
-        gameNumbers3 = new int[][]{{1, 2, 4, 3}, {4, 3, 1, 2}, {3, 1, 2, 4}, {2, 4, 3, 1}};
+        gameNumbers3 = new int[]{1, 2, 4, 3, 4, 3, 1, 2, 3, 1, 2, 4, 2, 4, 3, 1};
 
         // Array of all gameNumbers
-//        gameNumbers = new int[][][]{gameNumbers1, gameNumbers2, gameNumbers3};
+//        int[][] gameNumbers = new int[][]{gameNumbers1, gameNumbers2, gameNumbers3};
 
         // Assign ids to all edit texts
         boardViews = new EditText[][]{
@@ -98,7 +97,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Collection of rows within the grid
         rowArrays = new int[][]{row1, row2, row3, row4};
-        Log.i(TAG, "----> onCreate() : rowArrays.length = " + rowArrays.length);
 
         // Creating an array for each column to check users input
         int[] col1 = new int[]{R.id.edit_r1_c1, R.id.edit_r2_c1, R.id.edit_r3_c1, R.id.edit_r4_c1};
@@ -108,7 +106,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Collection of columns within the grid
         columnArrays = new int[][]{col1, col2, col3, col4};
-        Log.i(TAG, "----> onCreate() : columnArrays.length = " + columnArrays.length);
 
         // Creating an array for each section to check users input
         int[] section1_1 = new int[]{R.id.edit_r1_c1, R.id.edit_r1_c2, R.id.edit_r2_c1, R.id.edit_r2_c2};
@@ -118,15 +115,14 @@ public class MainActivity extends AppCompatActivity {
 
         // Creating an array for each corner to check users input
         sectionArrays = new int[][]{section1_1, section1_2, section2_1, section2_2};
-        Log.i(TAG, "----> onCreate() : sectionArrays.length = " + sectionArrays.length);
 
         /* ********************************* Game Boards *****************************************/
 
         // Getting the parameters for each board in order to assign to the views
         try {
-            sudokuBoard1 = new SudokuBoard(sudokuBoard1.getNumberArray1(), sudokuBoard1.getNumberArray2(), sudokuBoard1.getNumberArray3(), sudokuBoard1.getNumberArray4());
-            sudokuBoard2 = new SudokuBoard(sudokuBoard2.getNumberArray1(), sudokuBoard2.getNumberArray2(), sudokuBoard2.getNumberArray3(), sudokuBoard2.getNumberArray4());
-            sudokuBoard3 = new SudokuBoard(sudokuBoard3.getNumberArray1(), sudokuBoard3.getNumberArray2(), sudokuBoard3.getNumberArray3(), sudokuBoard3.getNumberArray4());
+            sudokuBoard1 = new SudokuBoard(sudokuBoard1.getNumberArrays());
+            sudokuBoard2 = new SudokuBoard(sudokuBoard2.getNumberArrays());
+            sudokuBoard3 = new SudokuBoard(sudokuBoard3.getNumberArrays());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -146,40 +142,42 @@ public class MainActivity extends AppCompatActivity {
 
                 for (int i = 0; i < boardViews.length; ++i) {
                     for (EditText[] boardView : boardViews) {
-                        for (int[] aGameNumbers1 : gameNumbers1) {
-                            if (boardView[i].getText().toString().isEmpty()) {
-                                boardView[i].setText(aGameNumbers1[gameNumbers1.length]);
-                                Log.i(TAG, "----> onClick(boardViews.length)" + boardViews.length);
-                            } else {
-                                // Show a Toast Dialog if edit views have empty ones
-                                Toast.makeText(MainActivity.this,
-                                        R.string.toastIncomplete, Toast.LENGTH_SHORT).show();
-                                return;
-                            }
+                        String numbers = boardView[i].getText().toString();
+                        if (numbers.isEmpty()) {
+                            boardView[i].setText((Arrays.toString(gameNumbers1)));
+
+                            System.out.println();
+                            Log.i(TAG, "----> onClick(boardViews.length)" + Arrays.toString(gameNumbers1));
+                            System.out.println();
+                        } else {
+                            // Show a Toast Dialog if edit views have empty ones
+                            Toast.makeText(MainActivity.this,
+                                    R.string.toastIncomplete, Toast.LENGTH_SHORT).show();
+                            return;
+                        }
 
                             /*Attempting to check users answers to each game board. I could not
                              *figure out how to figure in for the numbers that are initially visible.
                              *It seems efficient but I am also aware that all boards are not all visible.*/
 
-                            if (Objects.equals(String.valueOf(userNumbers[i]), String.valueOf(gameNumbers1[i])) ||
-                                    Objects.equals(String.valueOf(userNumbers[i]), String.valueOf(gameNumbers2[i])) ||
-                                    Objects.equals(String.valueOf(userNumbers[i]), String.valueOf(gameNumbers3[i]))) {
-                                // Show an Alert Dialog
-                                checkHasErrorsAlert();
-                            } else {
-                                // Show an Alert Dialog
-                                checkIsCorrectAlert();
-                            }
+                        if (Objects.equals(String.valueOf(userNumbers[i]), String.valueOf(gameNumbers1[i])) ||
+                                Objects.equals(String.valueOf(userNumbers[i]), String.valueOf(gameNumbers2[i])) ||
+                                Objects.equals(String.valueOf(userNumbers[i]), String.valueOf(gameNumbers3[i]))) {
+                            // Show an Alert Dialog
+                            checkHasErrorsAlert();
+                        } else {
+                            // Show an Alert Dialog
+                            checkIsCorrectAlert();
+                        }
 
-                            if (Arrays.deepToString(rowArrays).equals(String.valueOf(gameNumbers1[i][i])) ||
-                                    Arrays.deepToString(columnArrays).equals(String.valueOf(gameNumbers1[i][i])) ||
-                                    Objects.equals(String.valueOf(sectionArrays[i]), String.valueOf(gameNumbers1[i][i]))) {
-                                // Show an Alert Dialog for being not correct
-                                checkHasErrorsAlert();
-                            } else {
-                                // Show an Alert Dialog for being correct
-                                checkIsCorrectAlert();
-                            }
+                        if (Arrays.deepToString(rowArrays).equals(String.valueOf(gameNumbers1[i])) ||
+                                Arrays.deepToString(columnArrays).equals(String.valueOf(gameNumbers1[i])) ||
+                                Objects.equals(String.valueOf(sectionArrays[i]), String.valueOf(gameNumbers1[i]))) {
+                            // Show an Alert Dialog for being not correct
+                            checkHasErrorsAlert();
+                        } else {
+                            // Show an Alert Dialog for being correct
+                            checkIsCorrectAlert();
                         }
                     }
                 }
--- BoyntonAllen_CE04/app/src/main/java/edu/fullsail/aboynton/boyntonallen_ce04/SudokuBoard.java
+++ BoyntonAllen_CE04/app/src/main/java/edu/fullsail/aboynton/boyntonallen_ce04/SudokuBoard.java
@@ -8,33 +8,14 @@ package edu.fullsail.aboynton.boyntonallen_ce04;
 
 class SudokuBoard extends MainActivity {
 
-    private final int[] numberArray1;
-    private final int[] numberArray2;
-    private final int[] numberArray3;
-    private final int[] numberArray4;
+    private final int[] numberArrays;
 
-    SudokuBoard(int[] numberArray1, int[] numberArray2, int[] numberArray3, int[] numberArray4) {
-        this.numberArray1 = numberArray1;
-        this.numberArray2 = numberArray2;
-        this.numberArray3 = numberArray3;
-        this.numberArray4 = numberArray4;
+    public SudokuBoard(int[] numberArrays) {
+        this.numberArrays = numberArrays;
     }
 
-    int[] getNumberArray1() {
-
-        return numberArray1;
-    }
-
-    int[] getNumberArray2() {
-        return numberArray2;
-    }
-
-    int[] getNumberArray3() {
-        return numberArray3;
-    }
-
-    int[] getNumberArray4() {
-        return numberArray4;
+    public int[] getNumberArrays() {
+        return numberArrays;
     }
 }
 
